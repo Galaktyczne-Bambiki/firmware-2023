fc8220b6d7f19c0668ab9240941915113ac452d1
Marcin Bober <mbober1@gmail.com>
2023-10-07 15:40:08 +0200

    add temp

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,19 @@ constexpr byte M1_PIN = D1;
 constexpr byte TX_PIN = D5;
 constexpr byte RX_PIN = D6;
 constexpr byte AUX_PIN = D7;
-// constexpr byte DS18B20_PIN = 2;
+constexpr byte DS18B20_PIN = D4;
 // constexpr byte PMS_RX_PIN = 10;
 
 SoftwareSerial ESerial(RX_PIN, TX_PIN);
 EBYTE Transceiver(&ESerial, M0_PIN, M1_PIN, AUX_PIN);
-// DS18B20 ds(DS18B20_PIN);
+DS18B20 ds(DS18B20_PIN);
 // SoftwareSerial PMS_Serial(PMS_RX_PIN, -1);
 
 // PMS pms(PMS_Serial);
 // PMS::DATA data;
 
 struct DATA {
-  unsigned int temperature;
+  float temperature;
 } message;
 
 unsigned long Last;
@@ -36,23 +36,10 @@ void setup_e32(void)
   ESerial.begin(9600);            // EBYTE
 
 	Transceiver.init();
-  // Transceiver.SetSpeed(0);
-  // Transceiver.SetOptions(0);
 
 	Serial.println(Transceiver.GetAirDataRate());
   Serial.println(Transceiver.GetChannel());
 
-  // Transceiver.SetAirDataRate(ADR_1200);   // change the air data rate
-  // Transceiver.SetAddressH(1);   // set the high address byte
-  // Transceiver.SetAddressL(1);   // set the low address byte
-  // Transceiver.SetChannel(4);     // set the channel (0-32 is pretty typical)
-  // Transceiver.SetTransmitPower(0b00);
-  // Transceiver.SetTransmissionMode(MODE_NORMAL);
-  // Transceiver.SetMode(MODE_NORMAL);
-  // Transceiver.SetFECMode(OPT_FECENABLE);
-  // Transceiver.SetPullupMode(0x1);
-  // Transceiver.SaveParameters(TEMPORARY);  // save the parameters to the EBYTE EEPROM, you can save temp if periodic changes are needed
-  
   Transceiver.SetAddressH(0);
   Transceiver.SetAddressL(0);
   Transceiver.SetSpeed(0b00011100);  
@@ -65,7 +52,7 @@ void setup_e32(void)
 
 #ifdef RECEIVE
 void setup() {
-	Serial.begin(9600);             // Monitor
+	Serial.begin(9600);
   setup_e32();
 }
 
@@ -73,7 +60,7 @@ void loop() {
   if (ESerial.available()) 
 	{
     Transceiver.GetStruct(&message, sizeof(message));
-    Serial.printf("Got: %d\n\r", message.temperature);
+    Serial.printf("Got: %f\n\r", message.temperature);
     Last = millis();
   }
   else {
@@ -88,7 +75,7 @@ void loop() {
 
 #ifdef SEND
 void setup() {
-	Serial.begin(9600);             // Monitor
+	Serial.begin(9600);
   // PMS_Serial.begin(PMS::BAUD_RATE);  // PMS5003
   setup_e32();
 }
@@ -110,11 +97,9 @@ void loop()
   //   Serial1.println();
   // }
 
-  // message.temperature = ds.getTempC();
-  message.temperature = 255;
+  message.temperature = ds.getTempC();
   Transceiver.SendStruct(&message, sizeof(message));
-  // Transceiver.flush();
-  Serial.printf("Sending: %d\n\r", message.temperature);
-  delay(1000);
+  Serial.printf("Sending: %f\n\r", message.temperature);
+  delay(300);
 }
 #endif
